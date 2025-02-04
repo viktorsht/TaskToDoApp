adbfa3c88d3c9a84264decbb8afc5e0754dec54d
viktor <viktor.santos@minitok.live>
2025-02-04 16:10:36 -0300

    Implementando padrão result

--- lib/core/result.dart
+++ lib/core/result.dart
@@ -1,1 +1,26 @@
-abstract class Result <TSucess, TFailure> {}
+sealed class Result <TSucess, TFailure> {
+  
+  TSucess? getOrNull(){
+    if(this is Sucess) return (this as Sucess)._value;
+    return null;
+  }
+
+  TFailure? getOrNullFailure(){
+    if(this is Failure) return (this as Failure)._value;
+    return null;
+  }
+
+
+}
+
+class Sucess<TSucess, TFailure> extends Result<TSucess, TFailure> {
+  final TSucess _value;
+
+  Sucess(this._value);
+}
+
+class Failure<TSucess, TFailure> extends Result<TSucess, TFailure> {
+  final TSucess _value;
+
+  Failure(this._value);
+}
